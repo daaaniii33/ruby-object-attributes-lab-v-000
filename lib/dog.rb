dea7801cd43b8bcaf36532285c6853b85e880e9a
daaaniii33 <daaaniii33@gmail.com>
2018-11-27 05:28:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
-
 class Dog
-  def initialize
-    @this_dogs_name=name
-  end
-  def name=(name)
-    @this_dogs_name=name
+  def name=(dogs_name)
+    @name = dogs_name
   end
   def name
-    @this_dogs_name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
   end
 end
